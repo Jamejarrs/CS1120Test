df3439fa91ca2008d5cc7968abc7041442330d9c
CS User <CS User@c208-16>
2017-03-01 11:15:14 -0500

    Created object of Expression class

--- CS1120Test/src/edu/wmich/cs1120/test/TestMain.java
+++ CS1120Test/src/edu/wmich/cs1120/test/TestMain.java
@@ -3,7 +3,8 @@ package edu.wmich.cs1120.test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Expression exp = new Expression();
 
 	}
 
